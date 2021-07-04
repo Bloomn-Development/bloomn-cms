e5ecf30ca33f3820161c445bc54b4355693d166a
thenakulchawla <msg@nakul.net>
2021-07-03 22:12:08 -0700

    Add files for auth front end

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,21 @@
 import '../styles/global.scss'
 
-export default function App({ Component, pageProps }) {
-    return <Component {...pageProps} />
+// export default function App({ Component, pageProps }) {
+//     return <Component {...pageProps} />
+//   }
+
+import Navbar from '../components/Navbar'
+  
+  export default function MyApp({ Component, pageProps }) {
+    return (
+      <>
+        <Navbar />
+        <main>
+          <div className="container">
+            <Component {...pageProps} />
+          </div>
+        </main>
+      </>
+    )
   }
   
--- pages/index.js
+++ pages/index.js
@@ -1,49 +1,83 @@
-import Head from 'next/head'
-import Layout, { siteTitle } from '../components/layout'
-import utilStyles from '../styles/utils.module.scss'
-import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
+import { useUser, fetcher } from '../lib/hooks'
+import useSWR from 'swr'
 
-
-export async function getStaticProps() {
-  const allPostsData = getSortedPostsData()
-  return {
-    props: {
-      allPostsData
-    }
-  }
-}
-
-export default function Home({ allPostsData }) {
+function UserList() {
+  const { data: { users } = {} } = useSWR('/api/users', fetcher)
   return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
+    <>
+      <h2>All users</h2>
+      {!!users?.length && (
+        <ul>
+          {users.map((user) => (
+            <li key={user.username}>
+              <pre>{JSON.stringify(user, null, 2)}</pre>
             </li>
           ))}
+
+          <style jsx>{`
+            pre {
+              white-space: pre-wrap;
+              word-wrap: break-word;
+            }
+          `}</style>
         </ul>
-      </section>
-    </Layout>
+      )}
+    </>
+  )
+}
+
+export default function HomePage() {
+  const [user] = useUser()
+  return (
+    <>
+      <h1>
+        <a href="http://www.passportjs.org/">Passport.js</a> +{' '}
+        <a href="https://github.com/hoangvvo/next-connect">next-connect</a>{' '}
+        Example
+      </h1>
+      <h2>Steps to test the example:</h2>
+      <h3>Sign up</h3>
+      <ol>
+        <li>Click Sign up and enter a username and password.</li>
+        <li>You will be logged in and redirected home.</li>
+        <li>Click Logout. You will be redirected home.</li>
+        <li>
+          Try sign up again with the same username, you will see an error.
+        </li>
+      </ol>
+      <h3>Sign in</h3>
+      <ol>
+        <li>Click Login and login again using the same credential.</li>
+        <li>You will see an error if you use incorrect credential.</li>
+        <li>Otherwise, you will be authenticated and redirected home.</li>
+      </ol>
+      <h3>Edit profile</h3>
+      <ol>
+        <li>Click Profile</li>
+        <li>Enter a new name and click Update profile.</li>
+        <li>Notice how the name in Your profile has changed.</li>
+        <li>Click Delete profile</li>
+        <li>
+          The user is removed and is no longer shown in All users section in
+          Home
+        </li>
+      </ol>
+      {user && (
+        <>
+          <p>Currently logged in as:</p>
+          <pre>{JSON.stringify(user, null, 2)}</pre>
+        </>
+      )}
+      <UserList />
+      <style jsx>{`
+        li {
+          margin-bottom: 0.5rem;
+        }
+        pre {
+          white-space: pre-wrap;
+          word-wrap: break-word;
+        }
+      `}</style>
+    </>
   )
 }
